ca119bd9276f4771f74177a8f9288de7bf7c7ee8
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-09-28 22:08:59 +0100

    updated to to incorporate eslint

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,24 +1,14 @@
 
 export default function(Vue, options, {isClient}) {
+    if (!isClient) return
 
-   /* <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-151978506-1"></script>
-
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-151978506-1');
-    </script>*/
     function scriptBody(options) {
-     return   ` <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', '${options.id}');
-    </script>`
+        return `<script>
+               window.dataLayer = window.dataLayer || [];
+               function gtag(){dataLayer.push(arguments);}
+               gtag('js', new Date());
+               gtag('config', '${options.id}');
+               </script>`
     }
     document.head.insertAdjacentHTML('afterend', scriptBody(options))
     document.head.insertAdjacentHTML('afterend', `<script async src="https://www.googletagmanager.com/gtag/js?id=${options.id}"></script>`)
