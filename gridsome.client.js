5741509828fbca0bef68146b8ae7bd9b3f11f95a
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-09-28 21:37:50 +0100

    Removed gtag as it wasn't working

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,7 +1,27 @@
-import VueGtag from "vue-gtag";
+
 export default function(Vue, options, {isClient}) {
 
-Vue.use(VueGtag, {
-    config: { id: options.id }
-})
+   /* <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-151978506-1"></script>
+
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-151978506-1');
+    </script>*/
+    function scriptBody(options) {
+     return   ` <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${options.id}');
+    </script>`
+    }
+    document.head.insertAdjacentHTML('afterend', scriptBody(options))
+    document.head.insertAdjacentHTML('afterend', `<script async src="https://www.googletagmanager.com/gtag/js?id=${options.id}"></script>`)
+
+
 }
--- gridsome.server.js
+++ gridsome.server.js
@@ -1,4 +1,4 @@
-export default function(api, options) {
+module.exports = (api, options) => {
     api.setClientOptions(options)
-
 }
+
