54625ceca50d9a7510e36049316fd9de0f4b7f53
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-09-29 09:26:45 +0100

    move google tag to first element in head as instructions

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,15 +1,9 @@
-
+import VueGtagPlugin from "vue-gtag";
 export default function(Vue, options, {isClient}) {
     if (!isClient) return
 
-    function scriptBody(options) {
-        return `<script>
-               window.dataLayer = window.dataLayer || [];
-               function gtag(){dataLayer.push(arguments);}
-               gtag('js', new Date());
-               gtag('config', '${options.id}');
-               </script>`
-    }
-    document.head.insertAdjacentHTML('afterbegin', scriptBody(options))
-    document.head.insertAdjacentHTML('afterbegin', `<script async src="https://www.googletagmanager.com/gtag/js?id=${options.id}"></script>`)
+    Vue.use(VueGtagPlugin, {
+        "config": {"id": options.id}
+    })
+
 }
