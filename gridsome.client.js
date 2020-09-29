b1f705b77bdd4c5dad4a0b7ca7add2d300a4602d
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-09-29 20:01:07 +0100

    Updated to use router

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,9 +1,10 @@
 import VueGtagPlugin from "vue-gtag";
-export default function(Vue, options, {isClient}) {
+export default function(Vue, options, {isClient, router}) {
     if (!isClient) return
 
     Vue.use(VueGtagPlugin, {
-        "config": {"id": options.id}
-    })
+        "config": {"id": options.id},
+        "enabled": true
+    }, router)
 
 }
