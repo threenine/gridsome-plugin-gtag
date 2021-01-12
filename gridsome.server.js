2062c343edac3305333c98dd172971595c973457
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-01-12 17:18:11 +0000

    Dependency updates

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,4 +1,6 @@
 module.exports = (api, options) => {
+    // eslint-disable-next-line no-ternary
+    options.enabled = options.enabled === undefined ? process.env.NODE_ENV === 'production' : options.enabled
     api.setClientOptions(options)
 }
 
